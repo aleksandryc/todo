80341ca2bd0377f93a6d3c6f2511c11df4792ef7
aleksandryc <aleksandrb@eliaswoodwork.com>
2025-04-17 14:22:16 -0500

    Dropdown menu in nav begining

--- resources/js/pages/Shared/NavLink.vue
+++ resources/js/pages/Shared/NavLink.vue
@@ -3,16 +3,41 @@
     <Link class="hover:text-red-600" :class="{ 'text-red-400': page.url.startsWith('/users') }" href="/users">Users</Link>
     <Link class="hover:text-red-600" :class="{ 'text-red-400': page.url.startsWith('/settings') }" href="/settings">Settings</Link>
     <Link class="hover:text-red-600" :class="{ 'text-red-400': page.url.startsWith('/tasks') }" href="/tasks">Tasks</Link>
-    <Link class="hover:text-red-600" :class="{ 'text-red-400': page.url.startsWith('/name') }" href="/name/dashboard">Name</Link>
+    <div class="relative inline-block text-left">
+    <div class="group">
+        <button type="button"
+            class="inline-flex justify-center items-center w-full ">
+            <Link class="hover:text-red-600" :class="{ 'text-red-400': page.url.startsWith('/name')}" href="/name/dashboard" >Name</Link>
+            <!-- Dropdown arrow -->
+            <svg class="w-4 h-4 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 12l-5-5h10l-5 5z" />
+            </svg>
+        </button>
+
+        <!-- Dropdown menu -->
+        <div v-if="role === 'admin'"
+            class="absolute left-0 w-40 mt-1 origin-top-left bg-white divide-y divide-gray-100 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition duration-300">
+            <div class="py-1">
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Option 1</a>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Option 2</a>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Option 3</a>
+            </div>
+        </div>
+    </div>
+</div>
+
     <Link class="hover:text-red-600" href="/logout" method="post" as="button">Log out</Link>
 </template>
 
 <script setup lang="ts">
 import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 
 const page = usePage();
-defineProps({ active: Boolean });
+defineProps({ active: Boolean, });
+
+const role = computed(() => page.props.auth.role.roles);
 </script>
 
 <style lang="scss" scoped></style>
