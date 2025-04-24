cfed53e163de2caaa2103ebbc0c0a7f9b4e92487
aleksandryc <aleksandrb@eliaswoodwork.com>
2025-04-24 07:23:41 -0500

    worker page style added

--- resources/js/pages/Name/Worker/Index.vue
+++ resources/js/pages/Name/Worker/Index.vue
@@ -1,18 +1,13 @@
 <template>
     <AppHead title="Wrokers page" />
     <div class="container mx-auto" v-if="props.workshops">
-        <div class="flex justify-between mx-auto text-xs px-2">
+        <div class="mx-auto flex justify-between px-2 text-xs">
             <div v-for="shop in props.workshops" :key="shop.id">
-                <p class="text-center mb-2">{{ shop.workshop_name }}</p>
-                <div
-                v-for="table in shop.processes"
-                :key="table.id"
-                class="flex justify-between p-1 flex-wrap mr-2"
-                >{{ table.tables.name }} | {{ table.tables.color}}
+                <p class="mb-2 text-center font-bold text-xl tracking-widest border-2 border-amber-200 p-2 items-center shadow-amber-100 shadow-sm">{{ shop.workshop_name }}</p>
+                <div v-for="table in shop.processes" :key="table.id" class="mr-2 flex flex-wrap justify-between p-1">
+                    {{ table.tables.name }} | {{ table.tables.color }}
 
-                    <button
-                    @click.prevent="submit(table.tables.id)"
-                    class="ml-3 p-2 bg-amber-600 rounded-2xl text-xs font-light">Next step</button>
+                    <button @click.prevent="submit(table.tables.id)" class="ml-3 rounded-2xl bg-amber-600 p-2 text-xs font-light">Next step</button>
                 </div>
             </div>
         </div>
@@ -20,8 +15,8 @@
 </template>
 
 <script setup lang="ts">
-import AppHead from '../../Shared/AppHead.vue';
 import { router } from '@inertiajs/vue3';
+import AppHead from '../../Shared/AppHead.vue';
 
 const props = defineProps({
     workshops: Object,
