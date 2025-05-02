d1ba75b09e939bb7461e5cf5408b806a70f6908d
aleksandryc <aleksandrb@eliaswoodwork.com>
2025-05-02 09:58:26 -0500

    errors

--- resources/views/Forms/pdf.blade.php
+++ resources/views/Forms/pdf.blade.php
@@ -19,37 +19,51 @@
     <h1>{{ htmlspecialchars($title ?? 'Document') }}</h1>
 
     <table border="1" cellpadding="8" cellspacing="0" width="90%">
-        @if(!empty($fields) && is_array($fields))
-        @foreach($fields as $key => $value)
+
+        @forelse($fields as $key => $value)
         <tr>
-            <td><strong>{{ htmlspecialchars(ucwords(str_replace('_', ' ', $key))) }}</strong></td>
+            <td>
+                <strong>{{ htmlspecialchars(ucwords(str_replace('_', ' ', $key))) }}</strong>
+            </td>
             <td>
                 @php
                 $embeddedData = $embeddedImages[$key] ?? null;
                 @endphp
+
                 @if($embeddedData)
-                <a href="{{ asset('storage/'.$value) }}"><img src="{{ $embeddedData }}" alt="Embedded image"></a>
+                <a href="{{ asset('storage/'.$value) }}">
+                    <img src="{{ $embeddedData }}" alt="Embedded image">
+                </a>
 
                 @elseif(is_string($value) && str_starts_with($value, 'attachments/'))
-                <a href="{{ asset('storage/'.$value) }}">{{ asset('storage/'.$value) }}</a>
+                <a href="{{ asset('storage/'.$value) }}">
+                    {{ asset('storage/'.$value) }}
+                </a>
+                @elseif (is_array($value))
+                {{ implode(',', $value)}}
 
                 @else
-                @if (is_array($value))
-                {{ implode(',', $value)}}
-                @else
                 {{ htmlspecialchars($value) }}
-                @endif
+
                 @endif
             </td>
         </tr>
-        @endforeach
-        @else
+
+        @empty
         <tr>
             <td colspan="2">No data available</td>
         </tr>
-        @endif
+        @endforelse
     </table>
     <p>Submitted at: {{ now()->toDateTimeString() }}</p>
 </body>
 
 </html>
+
+
+/*
+TypeError
+htmlspecialchars(): Argument #1 ($string) must be of type string, array given
+vendor\laravel\framework\src\Illuminate\Support\helpers.php :141
+return htmlspecialchars($value ?? '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', $doubleEncode);
+*/
